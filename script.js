b29f869a08f35bc4908203c4384d6f38f11109bf
Siva Ranjini.C <101644016+siva-collab@users.noreply.github.com>
2024-02-09 20:23:51 +0530

    test

---
+++ script.js
@@ -0,0 +1,35 @@
+function showDemerits() {
+
+    window.location.href = "de.html";
+
+}
+
+function showRecipes() {
+
+    window.location.href = "re.html";
+
+}
+
+function showre1() {
+
+    window.location.href = "re1.html";
+
+}
+
+function showre2() {
+
+    window.location.href = "re2.html";
+
+}
+
+function showDos() {
+
+    window.location.href = "dos.html";
+
+}
+
+function showDonts() {
+
+    window.location.href = "donts.html";
+
+}
